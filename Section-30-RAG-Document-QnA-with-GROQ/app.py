6800adb1998fe46a6ee9b0df58de975d1cdf8612
MDalamin5 <mdal.amin5@northsouth.edu>
2024-11-28 20:24:44 +0600

    develop rag app using ollama and GORQ api

--- Section-30-RAG-Document-QnA-with-GROQ/app.py
+++ Section-30-RAG-Document-QnA-with-GROQ/app.py
@@ -9,16 +9,18 @@ from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.llms import Ollama
 
 
 from dotenv import load_dotenv
 load_dotenv()
-## load groq api key
-# os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
-groq_api_key = os.getenv("GROQ_API_KEY")
-os.environ["HF_TOKEN"]=os.getenv("HF_TOKEN")
+# ## load groq api key
+# # os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+# groq_api_key = os.getenv("GROQ_API_KEY")
+# os.environ["HF_TOKEN"]=os.getenv("HF_TOKEN")
 
-llm = ChatGroq(groq_api_key=groq_api_key, model_name="gemma2-9b-it")
+# llm = ChatGroq(groq_api_key=groq_api_key, model_name="gemma2-9b-it")
+llm = Ollama(model="gemma2:2b")
 
 prompt = ChatPromptTemplate.from_template(
     """
@@ -38,7 +40,7 @@ def create_vector_embedding():
     if "vectors" not in st.session_state:
         # st.session_state.embeddings = OllamaEmbeddings(model="gemma:2b") # ollama embedding
         st.session_state.embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-        st.session_state.loader = PyPDFDirectoryLoader("research_papers") ## data injection technique
+        st.session_state.loader = PyPDFDirectoryLoader("pdf_data") ## data injection technique
         st.session_state.docs = st.session_state.loader.load()
         st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=200)
         st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
