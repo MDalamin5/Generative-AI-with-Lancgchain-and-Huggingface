87766f177b00a28c1b3a82d4cca23ed74de22bb7
MDalamin5 <mdal.amin5@northsouth.edu>
2024-12-22 01:03:17 +0600

    done math solver using agentstypes

--- Section-36-Text-to-Math-problem-solver-Gemma2/app.py
+++ Section-36-Text-to-Math-problem-solver-Gemma2/app.py
@@ -40,5 +40,67 @@ calculator=Tool(
 )
 
 prompt="""
-Your a agent tasked to solving userrs math
+Your a agent tasked to solving users mathemtical questions. Logically arrive at the solution and provide a detailed explanation and display it point wise for the question below \n 
+Question: {question}
+Answer:
 """
+
+prompt_template = PromptTemplate(
+    input_variables=["question"],
+    template=prompt
+)
+
+
+## combine all the tools into chain
+chain = LLMChain(llm=llm, prompt=prompt_template)
+
+
+reasoning_tool = Tool(
+    name="Reasoning tool",
+    func=chain.run,
+    description="A tool for answering logic-based and reasoning question."
+)
+
+## initalize the agents
+assistant_agent=initialize_agent(
+    tools=[wikipedia_tool, calculator, reasoning_tool],
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=False,
+    handle_parsing_error=True
+)
+
+if "messages" not in st.session_state:
+    st.session_state['messages']=[
+        {"role":"assistant", "content":"Hi, i'm Math chatbot who can answer all your math questions."}
+    ]
+    
+for msg in st.session_state.messages:
+    st.chat_message(msg['role']).write(msg['content'])
+    
+## function to generate the response
+# def generate_response(question):
+#     response=assistant_agent.invoke({"input":question})
+#     return response
+
+
+## lets start the interaction
+question=st.text_area("Enter your question:","How to add tow number which is int and double")
+
+
+if st.button("Find my answer"):
+    if question:
+        with st.spinner("Generate Response..."):
+            st.session_state.messages.append({"role":"user","content":question})
+            st.chat_message("user").write(question)
+            
+            st_cb=StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = assistant_agent.run(st.session_state.messages, callbacks=[st_cb])
+            
+            st.session_state.messages.append({"role":'assistant',"content":response})
+            st.write("### Response:")
+            st.success(response)
+    else:
+        st.write("Please Enter your question")
+            
+            
